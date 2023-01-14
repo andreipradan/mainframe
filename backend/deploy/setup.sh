44605f1498e1a4a6145921b65620f03ee064d5f5
Andrei Prădan <andrei_pradan@external.mckinsey.com>
2023-01-14 16:06:38 +0200

    skip installing postgres deps every time

--- backend/deploy/setup.sh
+++ backend/deploy/setup.sh
@@ -5,7 +5,7 @@ PROJECT_DIR=${HOME}/projects/mainframe/backend
 VIRTUALENV_DIR=${HOME}/.virtualenvs/mainframe
 
 #echo "Setting crons..." && crontab "${PROJECT_DIR}/deploy/crons" && echo "Done."
-echo "Installing postgres deps..." && sudo apt-get -y install libpq-dev && echo "Done."
+# Skipped - only needed to run once - echo "Installing postgres deps..." && sudo apt-get -y install libpq-dev && echo "Done."
 
 [[ "$(ls -A "${VIRTUALENV_DIR}")" ]] && echo "Virtualenv already exists" || python -m venv "${VIRTUALENV_DIR}"
 "${VIRTUALENV_DIR}/bin/python" -m pip install -r "${PROJECT_DIR}/requirements.txt"
