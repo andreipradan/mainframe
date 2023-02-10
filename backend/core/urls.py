257c085a0314352550a3283f2fcb363cbd9dcead
Andrei Pradan <andrei.pradan@gmail.com>
2023-02-10 02:47:56 +0200

    butler mainframe

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 from api.urls import urlpatterns as api_urls
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(api_urls)),
+    path("", TemplateView.as_view(template_name="index.html")),
 ]
