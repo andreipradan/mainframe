9964992b1572a472df744991350477c5f68f63a1
Andrei Pradan <andrei.pradan@gmail.com>
2023-02-15 08:25:29 +0200

    be static root

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -146,10 +146,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "build" / "static",
-]
-STATIC_ROOT = BASE_DIR / "build" / "static"
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "build" / "static",
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
