8ea224d9160b07d4be8846c161afb4ca4cf0f6f8
Andrei Prădan <work@pradan.dev>
2023-03-23 12:43:34 +0200

    refactored set_hooks and send_debug_message mgm commands

--- backend/bots/management/commands/send_debug_message.py
+++ backend/bots/management/commands/send_debug_message.py
@@ -1,8 +1,9 @@
 import logging
 
-import environ
 import telegram
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
+
+from bots.models import Bot
 
 logger = logging.getLogger(__name__)
 
@@ -14,8 +15,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         msg = options["message"]
         logger.info(f"Sending debug message: {msg}")
-        telegram.Bot(environ.Env()("DEBUG_TOKEN")).send_message(
-            chat_id=environ.Env()("DEBUG_CHAT_ID"),
+        try:
+            bot = Bot.objects.get(additional_data__debug_chat_id__isnull=False)
+        except Bot.DoesNotExist:
+            raise CommandError(
+                "Bot with debug_chat_id in additional data does not exist"
+            )
+
+        chat_id = bot.additional_data["debug_chat_id"]
+        telegram.Bot(bot.token).send_message(
+            chat_id=chat_id,
             disable_notification=True,
             disable_web_page_preview=True,
             text=msg,
--- backend/bots/management/commands/set_hooks.py
+++ backend/bots/management/commands/set_hooks.py
@@ -29,44 +29,44 @@ def get_ngrok_url():
     raise LookupError("Tunnel URL not found")
 
 
+def set_github_hook(ngrok_url, access_token, username):
+    g = github.Github(access_token)
+    hook_config = {
+        "name": "web",
+        "config": {
+            "url": f"{ngrok_url}/api/hooks/github/",
+            "content_type": "json",
+            "secret": settings.SECRET_KEY,
+        },
+        "events": ["push"],
+        "active": True,
+    }
+    repository = g.get_repo(f"{username}/mainframe")
+    hooks = repository.get_hooks()
+
+    logger.warning(f"Deleting all hooks [{hooks.totalCount}]")
+    for hook in hooks:
+        hook.delete()
+
+    hook = repository.create_hook(**hook_config)
+    logger.info(f"Github web hook created successfully: {hook}")
+
+
+def set_telegram_hooks(ngrok_url):
+    logger.info("Setting telegram webhooks")
+    for bot in Bot.objects.filter(webhook_name__isnull=False).exclude(webhook_name=""):
+        try:
+            logger.info(
+                f"{bot.full_name}: {telegram.Bot(bot.token).set_webhook(f'{ngrok_url}/api/bots/{bot.id}/webhook/')}"
+            )
+        except telegram.error.TelegramError as e:
+            raise CommandError(str(e))
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         env = environ.Env()
-
         ngrok_url = get_ngrok_url()
-
-        g = github.Github(
-            env("GITHUB_ACCESS_TOKEN"),
-        )
-        hook_config = {
-            "name": "web",
-            "config": {
-                "url": f"{ngrok_url}/api/hooks/github/",
-                "content_type": "json",
-                "secret": settings.SECRET_KEY,
-            },
-            "events": ["push"],
-            "active": True,
-        }
-        repository = g.get_repo(f"{env('GITHUB_USERNAME')}/mainframe")
-        hooks = repository.get_hooks()
-
-        logger.warning(f"Deleting all hooks [{hooks.totalCount}]")
-        for hook in hooks:
-            hook.delete()
-
-        hook = repository.create_hook(**hook_config)
-        logger.info(f"Github web hook created successfully: {hook}")
-
-        logger.info("Setting telegram webhooks")
-        for bot in Bot.objects.filter(webhook_name__isnull=False).exclude(
-            webhook_name=""
-        ):
-            try:
-                logger.info(
-                    f"{bot.full_name}: {telegram.Bot(bot.token).set_webhook(f'{ngrok_url}/api/bots/{bot.id}/webhook/')}"
-                )
-            except telegram.error.TelegramError as e:
-                raise CommandError(str(e))
-
+        set_github_hook(ngrok_url, env("GITHUB_ACCESS_TOKEN"), env("GITHUB_USERNAME"))
+        set_telegram_hooks(ngrok_url)
         self.stdout.write(self.style.SUCCESS("Done."))
