01697efffc52f5331760793a7797e9e9c7bffe02
Andrei Prădan <work@pradan.dev>
2023-03-24 11:44:39 +0200

    be real refactoring

--- backend/bots/management/commands/be_real.py
+++ backend/bots/management/commands/be_real.py
@@ -87,8 +87,9 @@ class Command(BaseCommand):
             raise CommandError(
                 "Bot with be_real config in additional_data does not exist"
             )
-        instance.additional_data["be_real"]["caca"] = datetime.now().isoformat()
-        instance.save()
+        Bot.objects.filter(pk=instance.pk).update(
+            be_real__last_call=datetime.now().isoformat()
+        )
 
         be_real = instance.additional_data["be_real"]
         if not isinstance(be_real, dict) or not (chat_id := be_real.get("chat_id")):
