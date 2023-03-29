0a735c9e5b2cdc0d1baa324d6b8d32b3670b7994
Andrei Prădan <work@pradan.dev>
2023-03-29 21:20:41 +0300

    import transactions refactoring

--- backend/transactions/management/commands/import_transactions.py
+++ backend/transactions/management/commands/import_transactions.py
@@ -8,6 +8,7 @@ from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand, CommandError
 from django.db import IntegrityError
 
+from bots.models import Bot
 from transactions.models import Transaction
 
 logger = logging.getLogger(__name__)
@@ -37,9 +38,9 @@ class Command(BaseCommand):
         logger.info("Importing transactions")
 
         total = 0
-        for file_name in Path(settings.BASE_DIR / "transactions" / "data").glob(
-            "**/*.csv"
-        ):
+        data_path = settings.BASE_DIR / "transactions" / "data"
+        failed_imports = []
+        for file_name in Path(data_path).glob("**/*.csv"):
             with open(file_name) as file:
                 reader = csv.DictReader(file)
                 reader.fieldnames = list(map(get_field, reader.fieldnames))
@@ -51,16 +52,23 @@ class Command(BaseCommand):
                     results = Transaction.objects.bulk_create(transactions)
                 except ValidationError as e:
                     logger.error(f"{e}\nFile: {file_name.stem}")
+                    file_name.rename(f"{data_path}/{file_name.stem}.failed")
+                    failed_imports.append(file_name.stem)
                     continue
                 except IntegrityError as e:
                     logger.error(f"{e}\nFile: {file_name.stem}")
+                    file_name.rename(f"{data_path}/{file_name.stem}.failed")
+                    failed_imports.append(file_name.stem)
                     continue
                 else:
                     results_count = len(results)
                     logger.info(f"{file_name.stem}: {results_count} rows")
                     total += results_count
-
-            logger.info(f"Deleting {file_name.stem}")
-            file_name.unlink()
-
-        self.stdout.write(self.style.SUCCESS(f"Imported {total} transactions"))
+                    logger.info(f"Import completed - Deleting {file_name.stem}")
+                    file_name.unlink()
+        msg = f"Imported {total} transactions"
+        if failed_imports:
+            msg += f"\nFailed files: {', '.join(failed_imports)}"
+        bot = Bot.objects.get(additional_data__debug_chat_id__isnull=False)
+        bot.send_message(chat_id=bot.additional_data["debug_chat_id"], text=msg)
+        self.stdout.write(self.style.SUCCESS(msg))
