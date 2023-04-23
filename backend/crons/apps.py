694f7edc0480a0fee9de450fef57b57db15bebd1
andreipradan <andreipradan@users.noreply.github.com>
2023-04-23 19:06:20 +0000

    :art: Format Python code with psf/black

--- backend/crons/apps.py
+++ backend/crons/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CronsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'crons'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "crons"
