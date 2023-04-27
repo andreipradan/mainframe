135f659d657877fc43825ec1d673e17a837556af
andreipradan <andreipradan@users.noreply.github.com>
2023-04-27 01:40:06 +0000

    :art: Format Python code with psf/black

--- backend/crons/serializers.py
+++ backend/crons/serializers.py
@@ -24,15 +24,12 @@ class CronSerializer(serializers.ModelSerializer):
         return value
 
     def update(self, instance: Cron, validated_data):
-        need_to_update_cron = (
-            validated_data.get("is_active")
-            and (
-                instance.command != validated_data.get("command")
-                or instance.expression != validated_data.get("expression")
-                or (
-                    (args := validated_data.get("arguments"))
-                    and set(instance.arguments) != set(args)
-                )
+        need_to_update_cron = validated_data.get("is_active") and (
+            instance.command != validated_data.get("command")
+            or instance.expression != validated_data.get("expression")
+            or (
+                (args := validated_data.get("arguments"))
+                and set(instance.arguments) != set(args)
             )
         )
         instance = super().update(instance, validated_data)
