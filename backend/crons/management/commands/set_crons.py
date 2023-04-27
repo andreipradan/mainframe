685e0b0979c00a82f295ce74de73e7b595b2470c
Andrei Prădan <work@pradan.dev>
2023-04-27 12:32:08 +0300

    crons be

--- backend/crons/management/commands/set_crons.py
+++ backend/crons/management/commands/set_crons.py
@@ -1,14 +1,7 @@
-import logging
-from pathlib import Path
-
 from django.core.management.base import BaseCommand, CommandError
 
 from clients.cron import set_crons
-from core.settings import get_file_handler
 from crons.models import Cron
-
-logger = logging.getLogger(__name__)
-logger.addHandler(get_file_handler(Path(__file__).stem))
 
 
 class Command(BaseCommand):
