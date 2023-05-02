e6b34afdc15e4b6fb977c0f98c0e0cfb82ce771d
Andrei Prădan <andrei.pradan@gmail.com>
2023-05-02 20:28:34 +0300

    initial_setup (#88)
    Co-authored-by: Andrei Prădan <work@pradan.dev>

--- deploy/initial.sh
+++ deploy/initial.sh
@@ -1,6 +1,4 @@
 sudo apt-get update
 echo "Installing postgres deps..." && sudo apt-get -y install libpq-dev && echo "Done."
 sudo apt-get install zsh
-
-goto && git clone https://github.com/andreipradan/mainframe.git
-cd mainframe && ./deploy/setup.sh requirements restart
+goto mainframe/deploy && ./setup.sh requirements restart
