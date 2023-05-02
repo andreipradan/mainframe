a7db1db96390cccf52d9c2c6adb9600c3721dda3
Andrei Prădan <andrei.pradan@gmail.com>
2023-05-02 21:51:05 +0300

    initial_setup (#94)
    Co-authored-by: Andrei Prădan <work@pradan.dev>

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -7,7 +7,7 @@ LOGS_DIR=/var/log/mainframe/backend
 
 echo "$(date -u +"%Y-%m-%d %H:%M:%SZ") - Starting setup"
 
-([[ "$(ls -A "${LOGS_DIR}")" ]] && echo "$(date -u +"%Y-%m-%d %H:%M:%SZ") - [Logs] Path already exists") || (sudo mkdir -p "${LOGS_DIR}" && sudo touch "${LOGS_DIR}/backend.log" && echo "$(date -u +"%Y-%m-%d %H:%M:%SZ") - [Logs] Path created")
+([[ "$(ls -A "${LOGS_DIR}")" ]] && echo "$(date -u +"%Y-%m-%d %H:%M:%SZ") - [Logs] Path already exists") || (sudo mkdir -p "${LOGS_DIR}" && sudo touch "${LOGS_DIR}/backend.log" && sudo chown --recursive rpi ${LOGS_DIR} && echo "$(date -u +"%Y-%m-%d %H:%M:%SZ") - [Logs] Path created")
 
 [[ "$(ls -A "${VIRTUALENV_DIR}")" ]] && echo "$(date -u +"%Y-%m-%d %H:%M:%SZ") - [Virtualenv] Already exists" || (python -m venv "${VIRTUALENV_DIR}" && echo "[Virtualenv] Created")
 
