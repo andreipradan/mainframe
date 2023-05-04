3f180eb424f6fffe04ed54080d5b04c32fc9c536
Andrei Prădan <andrei.pradan@gmail.com>
2023-05-04 18:33:19 +0300

    initial_setup (#97)
    Co-authored-by: Andrei Prădan <work@pradan.dev>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: andreipradan <andreipradan@users.noreply.github.com>

--- backend/bots/management/commands/be_real.py
+++ backend/bots/management/commands/be_real.py
@@ -1,30 +1,20 @@
 import json
 import logging
 import random
-import time
 from datetime import datetime, timedelta
 from pathlib import Path
 from random import randrange
 
-import environ
-from crontab import CronTab
 from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
-from django.db import OperationalError
+from django.core.management.base import BaseCommand
 
 from bots.models import Bot
+from clients.cron import set_crons
 from core.settings import get_file_handler
+from crons.models import Cron
 
 logger = logging.getLogger(__name__)
 logger.addHandler(get_file_handler(Path(__file__).stem))
-
-DATETIME_FORMAT = "%H:%M %d.%m.%Y"
-PATH = f"{settings.LOGS_DIR}/crons/be-real/"
-COMMAND = (
-    f"mkdir -p {PATH}`date +%Y` && "
-    "$HOME/.virtualenvs/mainframe/bin/python $HOME/projects/mainframe/backend/manage.py be_real >> "
-    f"{PATH}`date +%Y`/`date +%Y-%m`.log 2>&1"
-)
 
 
 def get_tomorrow_run() -> datetime:
@@ -35,80 +25,32 @@ def get_tomorrow_run() -> datetime:
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "--post-deploy",
-            action="store_true",
-            default=False,
-            dest="post_deploy",
+    def handle(self, *args, **options):
+        instance = Bot.objects.get(additional_data__debug_chat_id__isnull=False)
+        logger.info("It's time to take a picture...")
+        data_path = settings.BASE_DIR / "bots" / "management" / "commands" / "data"
+
+        with open(data_path / "saluturi.json", "r") as salut_file:
+            salut = random.choice(json.load(salut_file))
+        with open(data_path / "actions.json", "r") as actions_file:
+            action = random.choice(json.load(actions_file))
+
+        text = f"❗️📷 {salut} {action} 📷❗️"
+        instance.send_message(
+            chat_id=instance.additional_data["be_real_chat_id"], text=text
         )
 
-    def handle(self, *args, **options):
-        config = environ.Env()
-        cron = CronTab(user=config("USERNAME"))
-        if (cmds_no := len(commands := list(cron.find_command("be_real")))) > 1:
-            crons = "\n".join(map(str, commands))
-            raise CommandError(f"Multiple 'be_real' crons found: {crons}")
-        if cmds_no < 1:
-            logger.info("No existing cron. Creating...")
-            cmd = cron.new(command=COMMAND)
-        else:
-            cmd = commands[0]
-            not cmd.enabled and cmd.enable() and logger.info("Enabling...")
-
-        try:
-            instance = Bot.objects.get(additional_data__be_real__isnull=False)
-        except OperationalError as e:
-            if options["post_deploy"] is False:
-                logger.info("Moving cron 1 minute later for retry")
-                cmd.minute = str(int(str(cmd.minute)) + 1)
-                cron.write()
-            raise CommandError(str(e))
-        except Bot.DoesNotExist:
-            raise CommandError("Bot with be_real config in additional_data missing")
-
-        be_real = instance.additional_data["be_real"]
-        if not isinstance(be_real, dict) or not (chat_id := be_real.get("chat_id")):
-            raise CommandError("Missing chat_id from be_real config")
-
-        if be_real.get("paused", False) is True:
-            if cmd.enabled and str(cmd.slices) != "* * * * *":
-                logger.info("Disabling...")
-                cmd.enable(False)
-                cron.write()
-            return logger.warning("be_real is paused.")
-
-        if options["post_deploy"] is False:
-            logger.info("It's time to take a picture...")
-            data_path = settings.BASE_DIR / "bots" / "management" / "commands" / "data"
-            with open(data_path / "saluturi.json", "r") as salut_file:
-                salut = random.choice(json.load(salut_file))
-            with open(data_path / "actions.json", "r") as actions_file:
-                action = random.choice(json.load(actions_file))
-            text = f"❗️📷 {salut} {action} 📷❗️"
-            instance.send_message(chat_id=chat_id, text=text)
-        else:
-            logger.info("Initializing be_real...")
-            if (next_run := (be_real.get("next_run"))) and (
-                next_run := (datetime.strptime(next_run, DATETIME_FORMAT))
-            ) >= datetime.today():
-                logger.info("Cron in future")
-                logger.info(f"Now: {datetime.today()}")
-                logger.info(f"Next run: {next_run}")
-                cmd.setall(
-                    f"{next_run.minute} {next_run.hour} {next_run.day} {next_run.month} *"
-                )
-                cron.write()
-                return logger.info(f"Set cron to {next_run}")
-
         tomorrow_run = get_tomorrow_run().replace(second=0, microsecond=0)
-        tomorrow_run_str = tomorrow_run.strftime(DATETIME_FORMAT)
         expression = f"{tomorrow_run.minute} {tomorrow_run.hour} {tomorrow_run.day} {tomorrow_run.month} *"
-        cmd.setall(expression)
-        cron.write()
-        time.sleep(3)
-        instance.additional_data["be_real"]["next_run"] = tomorrow_run_str
-        instance.save()
-        logger.info(f"Set next run and cron to {tomorrow_run_str}")
+        cron = Cron(
+            command=f"be_real",
+            expression=expression,
+            is_active=True,
+            is_management=True,
+        )
+        set_crons([cron])
+        logger.info(
+            f"Set next run and cron to {tomorrow_run.strftime('%H:%M %d.%m.%Y')}"
+        )
 
         return self.stdout.write(self.style.SUCCESS("Done."))
--- backend/bots/management/commands/send_healthcheck_ping.py
+++ backend/bots/management/commands/send_healthcheck_ping.py
@@ -1,21 +1,12 @@
-import logging
-from pathlib import Path
-
 import environ
 import requests
 from django.core.management.base import BaseCommand, CommandError
 from requests import HTTPError
 
-from core.settings import get_file_handler
-
-logger = logging.getLogger(__name__)
-logger.addHandler(get_file_handler(Path(__file__).stem))
-
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         config = environ.Env()
-        logger.info(f"Sending healthcheck ping")
         response = requests.post(url=config("HEALTHCHECKS_URL"))
         try:
             response.raise_for_status()
