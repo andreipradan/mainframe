a9826d4f484ec28fcb7acad0e008b6b05bb52e5b
Andrei Prădan <andrei.pradan@gmail.com>
2023-05-05 20:40:09 +0300

    channels (#108)
    Co-authored-by: Andrei Prădan <work@pradan.dev>

---
+++ deploy/.zshrc
@@ -0,0 +1,4 @@
+alias reload="source ~/.zshrc"
+alias showlogs="${HOME}/projects/mainframe/deploy/show-logs.sh"
+goto () {cd ~/projects/$1}
+cd ${HOME}/projects/mainframe
--- deploy/initial.sh
+++ deploy/initial.sh
@@ -2,6 +2,8 @@ echo "sudo apt-get update" && sudo apt-get update
 echo "Installing postgres deps..." && sudo apt-get -y install libpq-dev && echo "Done."
 echo "Installing zsh" && sudo apt-get install zsh
 echo "Installing ohmyzsh" && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Setting .zshrc aliases" && cat "${HOME}/projects/mainframe/deploy/.zshrc" >> "${HOME}/.zshrc"
+echo "Setting theme to af-magic" && sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="af-magic"/g' .zshrc
 echo "Installing ngrok"
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 echo "Setting ngrok tunnels" && cat "${HOME}/projects/mainframe/deploy/ngrok.yml" >> "${HOME}/.config/ngrok/ngrok.yml"
@@ -9,7 +11,6 @@ echo "Installing redis server" && sudo apt install redis-server
 echo "Installing homebridge"
 curl -sSfL https://repo.homebridge.io/KEY.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/homebridge.gpg  > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/homebridge.gpg] https://repo.homebridge.io stable main" | sudo tee /etc/apt/sources.list.d/homebridge.list > /dev/null
-sudo apt-get update
 sudo apt-get install homebridge
-echo "Installing drivers for picamera" && sudo apt-get install ffmpeg git python3-picamera python3-ws4py
+#echo "Installing drivers for picamera" && sudo apt-get install ffmpeg git python3-picamera python3-ws4py
 cd "${HOME}/projects/mainframe/deploy" && ./setup.sh requirements restart
