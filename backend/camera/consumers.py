33543a46d56b10d5d20bf6a5d19d8c0b4a508de9
Andrei Prădan <andrei.pradan@gmail.com>
2023-05-06 02:54:32 +0300

    refactoring (#112)
    Co-authored-by: Andrei Prădan <work@pradan.dev>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: andreipradan <andreipradan@users.noreply.github.com>

--- backend/camera/consumers.py
+++ backend/camera/consumers.py
@@ -1,30 +1,34 @@
+import io
 import json
 import logging
+import time
 
+import picamera
 from channels.generic.websocket import AsyncWebsocketConsumer
+from PIL import Image
 
 logger = logging.getLogger(__name__)
 
 
 class CameraConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        if not self.scope["user"].is_authenticated:
-            logger.warning("Unauthenticated, closing connection")
-            return await self.close()
-
         self.room_name = "camera"
         self.room_group_name = "chat_%s" % self.room_name
 
+        if not self.scope["user"].is_authenticated:
+            logger.warning("Unauthenticated, closing connection")
+            return await self.close()
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
         await self.accept()
+        self.stream_running = True
+        logger.info("stream running set to false")
 
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
-
-        # Receive message from WebSocket
+        self.stream_running = False
+        logger.info("stream running set to false")
 
     async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
@@ -35,10 +39,27 @@ class CameraConsumer(AsyncWebsocketConsumer):
             self.room_group_name, {"type": "chat_message", "message": message}
         )
 
-        # Receive message from room group
+    def video_stream(self, event):
+        self.send(text_data=event["text"])
+
+    def send_video_stream(self):
+        with picamera.PiCamera() as camera:
+            camera.resolution = (640, 480)
+            camera.framerate = 30
+
+            # Allow time for the camera to warm up
+            time.sleep(2)
+
+            while self.stream_running:
+                stream = io.BytesIO()
+                camera.capture(stream, format="jpeg", use_video_port=True)
+                stream.seek(0)
+                image = Image.open(stream)
+
+                # Convert the JPEG image to a byte array and send it to the WebSocket clients
+                buffer = io.BytesIO()
+                image.save(buffer, format="jpeg")
+                self.video_stream({"text": buffer.getvalue()})
 
     async def chat_message(self, event):
-        message = event["message"]
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({"message": event["message"]}))
