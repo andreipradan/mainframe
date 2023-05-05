bd51493cf4b56ae4020fdae0309ea93bcbfff2b7
andreipradan <andreipradan@users.noreply.github.com>
2023-05-05 21:26:35 +0000

    :art: Format Python code with psf/black

--- backend/camera/consumers.py
+++ backend/camera/consumers.py
@@ -8,7 +8,6 @@ logger = logging.getLogger(__name__)
 
 class CameraConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-
         self.room_name = "camera"
         self.room_group_name = "chat_%s" % self.room_name
 
