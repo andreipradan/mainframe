4cf5d4224615005999c30546ea41d3625a0035e8
Andrei Prădan <work@pradan.dev>
2023-05-06 11:31:32 +0300

    systemd service notifications

--- backend/crons/management/commands/set_crons.py
+++ backend/crons/management/commands/set_crons.py
@@ -1,7 +1,15 @@
+import logging
+import time
+from pathlib import Path
+
 from django.core.management.base import BaseCommand, CommandError
 
 from clients.cron import set_crons
+from core.settings import get_file_handler
 from crons.models import Cron
+
+logger = logging.getLogger(__name__)
+logger.addHandler(get_file_handler(Path(__file__).stem))
 
 
 class Command(BaseCommand):
@@ -14,8 +22,13 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        post_deploy = options["post_deploy"]
+        logger.info(f"Settings crons{' [post deploy]' if post_deploy else ''}")
+        if post_deploy:
+            logger.info("Post deploy 5s sleep before setting crons")
+            time.sleep(5)
         crons = Cron.objects.filter(is_active=True)
         if not crons:
             raise CommandError("No active crons in the database")
-        set_crons(crons, clear_all=options["post_deploy"])
+        set_crons(crons, clear_all=post_deploy)
         self.stdout.write(self.style.SUCCESS("Done."))
