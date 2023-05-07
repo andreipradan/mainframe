984b942c782fd68748dcacea688cc75138637a68
Andrei Pradan <andrei.pradan@gmail.com>
2023-05-08 00:53:20 +0300

    nginx config

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,7 +1,30 @@
-import os
-import django
-from channels.routing import get_default_application
+"""
+ASGI config for core project.
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-django.setup()
-application = get_default_application()
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+from camera import routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+        ),
+    }
+)
