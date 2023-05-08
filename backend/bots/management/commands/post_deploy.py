8c3dcea2bca208f0bc1508f69db5671fa3815954
Andrei Prădan <andrei.pradan@gmail.com>
2023-05-08 23:27:51 +0300

    post deploy (#131)
    Co-authored-by: Andrei Prădan <work@pradan.dev>

--- backend/bots/management/commands/post_deploy.py
+++ backend/bots/management/commands/post_deploy.py
@@ -53,41 +53,47 @@ def set_github_hook(ngrok_url, access_token, username):
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("--service", type=str, required=True)
+
     def handle(self, *args, **options):
-        env = environ.Env()
-        try:
-            ngrok_url = get_ngrok_url()
-        except ConnectionError:
-            raise CommandError("Failed to get ngrok tunnels. Is ngrok running?")
-        if not ngrok_url:
-            raise CommandError("Tunnel 'mainframe' not found")
+        service = options["service"]
+        if service == "backend":
+            logger.info(f"[Crons] Setting")
+            crons = Cron.objects.filter(is_active=True)
+            if not crons:
+                logger.error("No active crons in the database")
+            set_crons(crons, clear_all=True)
+            logger.info(f"[Crons] Done")
 
-        logger.info(f"[GitHub] Setting hook")
-        hook = set_github_hook(
-            ngrok_url, env("GITHUB_ACCESS_TOKEN"), env("GITHUB_USERNAME")
-        )
-        logger.info(f"[GitHub] Web hook created successfully: {hook}")
-
-        logger.info("[Telegram] Setting hooks")
-        for bot in Bot.objects.filter(is_active=True):
+            healthchecks.ping() and logger.info("[Healthcheck] Done")
+        elif service == "ngrok":
+            env = environ.Env()
             try:
-                response = bot.telegram_bot.set_webhook(
-                    f"{ngrok_url}/api/bots/{bot.id}/webhook/"
-                )
-                logger.info(f"{bot.full_name}: {response}")
-            except telegram.error.TelegramError as e:
-                logger.error(str(e))
-        logger.info("[Telegram hooks] Done")
+                ngrok_url = get_ngrok_url()
+            except ConnectionError:
+                raise CommandError("Failed to get ngrok tunnels. Is ngrok running?")
+            if not ngrok_url:
+                raise CommandError("Tunnel 'mainframe' not found")
 
-        logger.info(f"[Crons] Setting")
-        crons = Cron.objects.filter(is_active=True)
-        if not crons:
-            logger.error("No active crons in the database")
-        set_crons(crons, clear_all=True)
-        logger.info(f"[Crons] Done")
+            logger.info(f"[GitHub] Setting hook")
+            hook = set_github_hook(
+                ngrok_url, env("GITHUB_ACCESS_TOKEN"), env("GITHUB_USERNAME")
+            )
+            logger.info(f"[GitHub] Web hook created successfully: {hook}")
 
-        healthchecks.ping() and logger.info("[Healthcheck] Done")
+            logger.info("[Telegram] Setting hooks")
+            for bot in Bot.objects.filter(is_active=True):
+                try:
+                    response = bot.telegram_bot.set_webhook(
+                        f"{ngrok_url}/api/bots/{bot.id}/webhook/"
+                    )
+                    logger.info(f"{bot.full_name}: {response}")
+                except telegram.error.TelegramError as e:
+                    logger.error(str(e))
+            logger.info("[Telegram hooks] Done")
+        else:
+            logger.error(f"Invalid service: {service}")
 
-        send_telegram_message("[backend] up")
-
+        send_telegram_message(f"[{service}] up")
         self.stdout.write(self.style.SUCCESS("[post-deploy] Done."))
