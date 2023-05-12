bccc224dd6ab31c05c2d5a8a54d3fae291940309
Andrei Prădan <andrei.pradan@gmail.com>
2023-05-12 21:54:07 +0300

    raspberry (#148)
    Co-authored-by: Andrei Prădan <work@pradan.dev>

---
+++ backend/bots/management/commands/set_hooks.py
@@ -0,0 +1,77 @@
+import logging
+from pathlib import Path
+
+import environ
+import github
+import requests
+import telegram
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from requests.exceptions import ConnectionError
+
+from bots.models import Bot
+from clients import healthchecks
+from clients.cron import set_crons
+from clients.chat import send_telegram_message
+from core.settings import get_file_handler
+from crons.models import Cron
+
+logger = logging.getLogger(__name__)
+logger.addHandler(get_file_handler(Path(__file__).stem))
+
+
+def get_ngrok_url(name="mainframe"):
+    logger.info("Getting ngrok tunnels")
+    resp = requests.get("http://localhost:4040/api/tunnels").json()
+    for tunnel in resp["tunnels"]:
+        if tunnel["name"] == name:
+            return tunnel["public_url"]
+    else:
+        logger.warning(f"Tunnel not found: {name}")
+
+
+def set_github_hook(ngrok_url):
+    env = environ.Env()
+    g = github.Github(env("GITHUB_ACCESS_TOKEN"))
+    hook_config = {
+        "name": "web",
+        "config": {
+            "url": f"{ngrok_url}/api/hooks/github/",
+            "content_type": "json",
+            "secret": settings.SECRET_KEY,
+        },
+        "events": ["push", "workflow_run"],
+        "active": True,
+    }
+    repository = g.get_repo(f"{env('GITHUB_USERNAME')}/mainframe")
+    hooks = repository.get_hooks()
+
+    logger.warning(f"[GitHub] Deleting all hooks [{hooks.totalCount}]")
+    for hook in hooks:
+        hook.delete()
+
+    return repository.create_hook(**hook_config)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        try:
+            ngrok_url = get_ngrok_url()
+        except ConnectionError:
+            raise CommandError("Failed to get ngrok tunnels. Is ngrok running?")
+        if not ngrok_url:
+            raise CommandError("Tunnel 'mainframe' not found")
+
+        logger.info("[Hooks][GitHub] Setting hook")
+        logger.info(f"[Hooks][GitHub] Done: {set_github_hook(ngrok_url)}")
+        logger.info("[Hooks][Telegram] Setting hooks")
+        for bot in Bot.objects.filter(is_active=True):
+            url = f"{ngrok_url}/api/bots/{bot.id}/webhook/"
+            try:
+                response = bot.telegram_bot.set_webhook(url)
+                logger.info(f"[Hooks][Telegram] {bot.full_name}: {response}")
+            except telegram.error.TelegramError as e:
+                logger.error(str(e))
+        logger.info("[Hooks] Done")
+        send_telegram_message(f"[[ngrok]] up: {ngrok_url}")
+        self.stdout.write(self.style.SUCCESS("[Hooks] Done."))
