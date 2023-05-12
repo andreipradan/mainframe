67efc868dbaccf7537d89b37744b62e589daa069
Andrei Prădan <work@pradan.dev>
2023-05-12 11:56:19 +0300

    systemd files

--- backend/camera/views.py
+++ backend/camera/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from camera.consumers import CameraConsumer
+# from camera.consumers import CameraConsumer
 
 logger = logging.getLogger(__name__)
 
