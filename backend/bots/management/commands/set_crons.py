99f53f62f7e7e73270c75da961f6512eaf849975
Andrei Prădan <work@pradan.dev>
2023-05-13 19:45:01 +0300

    added gunicorn

--- backend/bots/management/commands/set_crons.py
+++ backend/bots/management/commands/set_crons.py
@@ -1,10 +1,6 @@
 import logging
 from pathlib import Path
 
-import environ
-import github
-import requests
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from clients import healthchecks
@@ -15,39 +11,6 @@ from crons.models import Cron
 
 logger = logging.getLogger(__name__)
 logger.addHandler(get_file_handler(Path(__file__).stem))
-
-
-def get_ngrok_url(name="mainframe"):
-    logger.info("Getting ngrok tunnels")
-    resp = requests.get("http://localhost:4040/api/tunnels").json()
-    for tunnel in resp["tunnels"]:
-        if tunnel["name"] == name:
-            return tunnel["public_url"]
-    else:
-        logger.warning(f"Tunnel not found: {name}")
-
-
-def set_github_hook(ngrok_url):
-    env = environ.Env()
-    g = github.Github(env("GITHUB_ACCESS_TOKEN"))
-    hook_config = {
-        "name": "web",
-        "config": {
-            "url": f"{ngrok_url}/api/hooks/github/",
-            "content_type": "json",
-            "secret": settings.SECRET_KEY,
-        },
-        "events": ["push"],
-        "active": True,
-    }
-    repository = g.get_repo(f"{env('GITHUB_USERNAME')}/mainframe")
-    hooks = repository.get_hooks()
-
-    logger.warning(f"[GitHub] Deleting all hooks [{hooks.totalCount}]")
-    for hook in hooks:
-        hook.delete()
-
-    return repository.create_hook(**hook_config)
 
 
 class Command(BaseCommand):
