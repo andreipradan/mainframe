53f451ceb2cf2c2f63366f213cf6ce8e0efc6bf5
Andrei Prădan <work@pradan.dev>
2023-05-16 18:46:27 +0300

    disabled nginx

--- deploy/initial.sh
+++ deploy/initial.sh
@@ -67,23 +67,23 @@ echo "[env] Done."
 
 echo "[postgres] Installing postgres deps..." && sudo apt-get -y install libpq-dev && echo "[postgres] Done."
 
-NGINX_DIR=/etc/nginx
-NGINX_AVAILABLE="${NGINX_DIR}/sites-available/mainframe"
-NGINX_ENABLED="${NGINX_DIR}/sites-enabled/mainframe"
-NGINX_ENABLED_DEFAULT="${NGINX_DIR}/sites-enabled/default"
-PROJECT_DIR=${HOME}/projects/mainframe
-echo "[nginx] Installing nginx" && sudo apt-get -y install nginx
-echo "[nginx] Adding configuration"
-if [ -f "$NGINX_AVAILABLE" ]; then sudo rm $NGINX_AVAILABLE && echo "Deleted ${NGINX_AVAILABLE}"; else echo "File not found: ${NGINX_AVAILABLE}"; fi
-if [ -f "$NGINX_ENABLED" ]; then sudo rm $NGINX_ENABLED && echo "Deleted ${NGINX_ENABLED}"; else echo "File not found: ${NGINX_ENABLED}"; fi
-if [ -f "$NGINX_ENABLED_DEFAULT" ]; then sudo rm $NGINX_ENABLED_DEFAULT && echo "Deleted ${NGINX_ENABLED_DEFAULT}"; else echo "File not found: ${NGINX_ENABLED_DEFAULT}"; fi
-sudo touch ${NGINX_AVAILABLE}
-sudo chown rpi $NGINX_AVAILABLE
-cat "${PROJECT_DIR}/deploy/nginx.conf" >> $NGINX_AVAILABLE
-sudo ln -s ${NGINX_AVAILABLE} $NGINX_DIR/sites-enabled
-sudo nginx -t
-sudo systemctl restart nginx
-echo "[nginx] Done."
+#NGINX_DIR=/etc/nginx
+#NGINX_AVAILABLE="${NGINX_DIR}/sites-available/mainframe"
+#NGINX_ENABLED="${NGINX_DIR}/sites-enabled/mainframe"
+#NGINX_ENABLED_DEFAULT="${NGINX_DIR}/sites-enabled/default"
+#PROJECT_DIR=${HOME}/projects/mainframe
+#echo "[nginx] Installing nginx" && sudo apt-get -y install nginx
+#echo "[nginx] Adding configuration"
+#if [ -f "$NGINX_AVAILABLE" ]; then sudo rm $NGINX_AVAILABLE && echo "Deleted ${NGINX_AVAILABLE}"; else echo "File not found: ${NGINX_AVAILABLE}"; fi
+#if [ -f "$NGINX_ENABLED" ]; then sudo rm $NGINX_ENABLED && echo "Deleted ${NGINX_ENABLED}"; else echo "File not found: ${NGINX_ENABLED}"; fi
+#if [ -f "$NGINX_ENABLED_DEFAULT" ]; then sudo rm $NGINX_ENABLED_DEFAULT && echo "Deleted ${NGINX_ENABLED_DEFAULT}"; else echo "File not found: ${NGINX_ENABLED_DEFAULT}"; fi
+#sudo touch ${NGINX_AVAILABLE}
+#sudo chown rpi $NGINX_AVAILABLE
+#cat "${PROJECT_DIR}/deploy/nginx.conf" >> $NGINX_AVAILABLE
+#sudo ln -s ${NGINX_AVAILABLE} $NGINX_DIR/sites-enabled
+#sudo nginx -t
+#sudo systemctl restart nginx
+#echo "[nginx] Done."
 
 echo "[ngrok] Installing ngrok"
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
