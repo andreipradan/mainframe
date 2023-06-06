8a11b89dfd496f09453032f1a6fc4eae3a3d793c
Andrei Prădan <work@pradan.dev>
2023-06-06 21:05:05 +0300

    earthquakes fix

--- backend/earthquakes/management/commands/check_infp.py
+++ backend/earthquakes/management/commands/check_infp.py
@@ -14,27 +14,49 @@ class Command(BaseEarthquakeCommand):
     logger = logging.getLogger(__name__)
     logger.addHandler(ManagementCommandsHandler())
     source = Earthquake.SOURCE_INFP
-    url = "https://dataportal.infp.ro/?proximityMax=2"
+    url = "http://n1.infp.ro/"
 
     def fetch(self, **options):
-        return requests.get(self.url, timeout=30, verify=False)
+        return requests.get(self.url, timeout=30)
 
     def fetch_events(self, response):
         soup = BeautifulSoup(response.text, features="html.parser")
-        return soup.html.body.find_all("div", {"class": "event-item"})
+        return soup.html.body.find_all("div", {"class": "card"})
 
-    def parse_earthquake(self, card) -> Earthquake:
-        intensity = card.find("span", {"title": "Intensitate epicentrala"})
-        timestamp = datetime.strptime(card.attrs["data-time"], "%Y-%m-%d %H:%M:%S")
+    def get_datetime(self, string):
+        date, time, tz = string.split()
+        return datetime.strptime(f"{date} {time}", "%d.%m.%Y, %H:%M:%S").replace(
+            tzinfo=pytz.timezone(tz)
+        )
+
+    def parse_earthquake(self, card):
+        body = card.find("div", {"class": "card-body"})
+        text, *rest = body.find_all("p")
+        lat = (
+            body.find("span", {"title": "Latitudine"})
+            .text.strip("\n Latitudine")
+            .strip(" °N")
+        )
+        long = (
+            body.find("span", {"title": "Longitudine"})
+            .text.strip("\n Longitudine")
+            .strip(" °E")
+        )
+        magnitude, *location = (
+            card.find("div", {"class": "card-footer"}).text.strip().split(", ")
+        )
+        timestamp = (
+            card.find("div", {"class": "card-header"})
+            .text.replace("Loading...", "")
+            .strip()
+        )
         return Earthquake(
-            timestamp=timestamp.replace(tzinfo=pytz.utc),
-            depth=card.find("span", {"title": "labels.depth"})
-            .text.strip()
-            .replace("km", ""),
-            intensity=intensity.text.strip() if intensity else None,
-            latitude=card.attrs["data-lat"],
-            longitude=card.attrs["data-lon"],
-            location=card.find("span").text.strip(),
-            magnitude=card.attrs["data-magnitude"],
+            timestamp=self.get_datetime(timestamp),
+            depth=text.text.strip().split("la adâncimea de ")[1][:-1].split(" ")[0],
+            intensity=rest[0].text.strip().split()[1] if len(rest) > 1 else None,
+            latitude=lat,
+            longitude=long,
+            location=",".join(location),
+            magnitude=magnitude.split()[1],
             source=Earthquake.SOURCE_INFP,
         )
