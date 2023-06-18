6dccaf890a3fe28bd11abce4b97d47fe9168e1f0
Andrei Prădan <work@pradan.dev>
2023-06-19 00:39:42 +0300

    queue logging handlers

--- backend/clients/logs.py
+++ backend/clients/logs.py
@@ -1,10 +1,14 @@
 import json
 import logging
+import queue
 from functools import cached_property
 from logging.handlers import MemoryHandler
 
 import environ
 import axiom
+
+FORMAT = "%(asctime)s - %(levelname)s - %(module)s.%(name)s - %(msg)s"
+logging.basicConfig(format=FORMAT, level=logging.INFO)
 
 
 def parse_value(value):
@@ -20,7 +24,7 @@ def parse_record(record: logging.LogRecord) -> dict:
         field: parse_value(getattr(record, field, None))
         for field in record.__dict__.keys()
     }
-    record["msg"] = f"[Mainframe] {record['msg']}"
+    record["message"] = f"[Mainframe] {record['message']}"
     return record
 
 
@@ -28,28 +32,32 @@ class AxiomHandler(logging.Handler):
     def __init__(self, dataset):
         super().__init__()
         self.dataset = dataset
+        self.setFormatter(logging.Formatter(fmt=FORMAT))
 
     @cached_property
     def client(self):
         return axiom.Client(environ.Env()("AXIOM_TOKEN"))
 
     def emit(self, record: logging.LogRecord) -> None:
+        self.format(record)
         self.client.ingest_events(self.dataset, [parse_record(record)])
 
 
-class ManagementCommandsHandler(MemoryHandler):
+class ManagementCommandsHandler(AxiomHandler):
     def __init__(self):
-        super().__init__(
-            capacity=5,
-            flushLevel=logging.INFO,
-            target=AxiomHandler("management"),
-        )
+        super().__init__("management")
 
 
-class MainframeHandler(MemoryHandler):
+class MainframeHandler(AxiomHandler):
     def __init__(self):
-        super().__init__(
-            capacity=5,
-            flushLevel=logging.INFO,
-            target=AxiomHandler("mainframe"),
-        )
+        super().__init__("mainframe")
+
+
+def get_logger(name):
+    from logging.handlers import QueueHandler, QueueListener
+
+    que = queue.Queue(-1)
+    logger = logging.getLogger(name)
+    logger.addHandler(QueueHandler(que))
+    listener = QueueListener(que, MainframeHandler())
+    return logger, listener
