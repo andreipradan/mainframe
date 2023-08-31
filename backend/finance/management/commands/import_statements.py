4645d745159d5925ef765e0b05dc7a82d393a3c5
Andrei Prădan <work@pradan.dev>
2023-08-31 16:46:16 +0300

    finance_backup updates

--- backend/finance/management/commands/import_statements.py
+++ backend/finance/management/commands/import_statements.py
@@ -266,5 +266,5 @@ class Command(BaseCommand):
         send_telegram_message(text=msg)
 
         self.stdout.write(self.style.SUCCESS(msg))
-        cron.delay("backup_finance --model=Account", minutes=2)
+        cron.delay("backup_finance --model=Account")
         cron.delay("backup_finance --model=Transaction")
