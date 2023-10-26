5f6fd2a09d0d72586039b57819cbf9991faee4e8
Andrei Pradan <work@pradan.dev>
2023-10-26 18:14:45 +0300

    moving usgs earthquake check to huey

--- backend/earthquakes/tasks.py
+++ backend/earthquakes/tasks.py
@@ -4,6 +4,12 @@ from huey.contrib.djhuey import periodic_task, HUEY
 
 
 @periodic_task(crontab(minute="*/1"))
-@HUEY.lock_task("reports-lock")
+@HUEY.lock_task("infp-lock")
 def check_infp():
     call_command("check_infp")
+
+
+@periodic_task(crontab(minute="*/1"))
+@HUEY.lock_task("usgs-lock")
+def check_usgs():
+    call_command("check_usgs")
