9be4eddfb9ed0f809cc0787f61e5e995fdd996ef
Andrei Pradan <work@pradan.dev>
2024-02-05 20:11:15 +0200

    moving healthcheck in huey

--- backend/bots/tasks.py
+++ backend/bots/tasks.py
@@ -3,15 +3,22 @@ from collections import deque
 from django.conf import settings
 from django.core.management import call_command
 from huey import crontab
-from huey.contrib.djhuey import HUEY, db_periodic_task
+from huey.contrib.djhuey import HUEY, db_periodic_task, periodic_task
 
 from bots.models import Bot
+from clients import healthchecks
 
 
 @db_periodic_task(crontab(minute=59, hour=23, day=2))
 @HUEY.lock_task("backup-bots-lock")
 def backup_bots():
     call_command("backup", app="bots")
+
+
+@periodic_task(crontab(minute="*/5"))
+@HUEY.lock_task("healthcheck-lock")
+def healthcheck():
+    healthchecks.ping()
 
 
 @db_periodic_task(crontab(minute=0, hour=19))
