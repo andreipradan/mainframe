d5ab7236d58ef6c14fd4698d57f9cbefc59c9df0
Andrei Pradan <work@pradan.dev>
2024-03-18 09:49:59 +0200

    run_watchers cron

--- backend/watchers/tasks.py
+++ backend/watchers/tasks.py
@@ -2,23 +2,15 @@ import logging
 
 from django.core.management import call_command
 from huey import crontab
-from huey.contrib.djhuey import HUEY, db_periodic_task, db_task
+from huey.contrib.djhuey import HUEY, db_periodic_task
 
 from clients.logs import ManagementCommandsHandler
-from watchers.models import Watcher
 
 logger = logging.getLogger(__name__)
 logger.addHandler(ManagementCommandsHandler())
 
 
-@db_task(expires=30)
-def run_watcher(watcher_id):
-    call_command("run_watcher", args=[watcher_id])
-
-
 @db_periodic_task(crontab(minute=0, hour=7))
 @HUEY.lock_task("run-watchers-lock")
 def run_watchers():
-    watchers = Watcher.objects.filter(is_active=True)
-    for watcher in watchers:
-        run_watcher(watcher.id)
+    call_command("run_watchers")
