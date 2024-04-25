a9675d2dbbfa884c980acbf66bb9339aa1a2c46d
Andrei Pradan <work@pradan.dev>
2024-04-26 00:36:11 +0300

    gcp be deploy # 1

---
+++ backend/gunicorn.config.py
@@ -0,0 +1,9 @@
+import multiprocessing
+
+bind = "0.0.0.0:$PORT"
+chdir = "/app"
+log_file = "-"
+max_requests = 1000
+max_requests_jitter = 50
+workers = multiprocessing.cpu_count() * 2 + 1
+wsgi_app = "core.wsgi:application"
