a2a22eae1c9c9570b5c7257fe780342bcb65e3ce
Andrei Pradan <work@pradan.dev>
2024-04-26 18:13:40 +0300

    don't send telegram message on fetching exchage rates

--- backend/exchange/management/commands/fetch_exchange_rates.py
+++ backend/exchange/management/commands/fetch_exchange_rates.py
@@ -3,9 +3,8 @@ import logging
 from django.core.management.base import BaseCommand, CommandError
 
 from clients import healthchecks
-from clients.chat import send_telegram_message
 from clients.logs import ManagementCommandsHandler
-from exchange.management.clients import FetchExchangeRatesException, BNR, ECB
+from exchange.management.clients import BNR, ECB, FetchExchangeRatesException
 
 CLIENTS = {"bnr": BNR, "ecb": ECB}
 
@@ -29,15 +28,14 @@ class Command(BaseCommand):
         logger.addHandler(ManagementCommandsHandler())
 
         source = options["source"]
-        logger.info(f"Fetching {source.upper()} exchange rates")
+        logger.info("Fetching %s exchange rates", source.upper())
         healthchecks.ping(f"{source}-fx")
 
         try:
             count = CLIENTS[source](logger).fetch(full=options["full"])
         except FetchExchangeRatesException as e:
-            raise CommandError(e)
+            raise CommandError(e) from e
 
         msg = f"Fetched {count} {source.upper()} exchange rates"
         logger.info(msg)
-        send_telegram_message(text=msg)
         self.stdout.write(self.style.SUCCESS("Done."))
