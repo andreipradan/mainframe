faeaac97d36a40b70ba83fa9c4da5d58b8bbe043
Andrei Pradan <work@pradan.dev>
2024-05-08 00:43:58 +0300

    removing api path from backend urls

--- src/mainframe/core/urls.py
+++ src/mainframe/core/urls.py
@@ -1,13 +1,11 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path, re_path
-from django.views.generic import TemplateView
-from mainframe.api.urls import urlpatterns as api_urls
+from django.urls import include, path
+from mainframe.api.urls import urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(api_urls)),
-    re_path(r".*", TemplateView.as_view(template_name="index.html")),
+    path("", include(urlpatterns)),
 ]
 if settings.ENV == "local":
     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
