7926bf2b08748bab54ad6c684035c01a53e78bca
andrei.pradan <work@pradan.dev>
2025-02-12 13:05:50 +0200

    FE: lights fix

--- frontend/src/api/lights.js
+++ frontend/src/api/lights.js
@@ -1,5 +1,4 @@
-// import {ngrokAxios as axios} from "./index";
-import axios from "./index";
+import {ngrokAxios as axios} from "./index";
 import {
   set,
   setBrightness,
@@ -20,7 +19,7 @@ import { ListApi, mix, TokenMixin } from './shared';
 class LightsApi extends mix(ListApi, TokenMixin) {
   static baseUrl = "lights"
   static methods = {set, setErrors, setLoading}
-  // static ngrokAxios = ["getList"]
+  static ngrokAxios = ["getList"]
 
   static setBrightness = (token, lightIp, brightness) => (dispatch) => {
     dispatch(setLoadingItems(lightIp));
