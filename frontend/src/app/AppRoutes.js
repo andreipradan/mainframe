7153b77c8cdc2f5be0ca3c8c4b11472663ba98ae
Andrei <work@pradan.dev>
2025-10-05 17:05:20 +0300

    fx rates chart fix

--- frontend/src/app/AppRoutes.js
+++ frontend/src/app/AppRoutes.js
@@ -2,7 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Spinner from '../app/shared/Spinner';
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 
 const Dashboard = lazy(() => import('./dashboard/Dashboard'));
 const Bots = lazy(() => import('./bots/Bots'));
@@ -12,29 +12,39 @@ const Devices = lazy(() => import('./devices/Devices'));
 const Earthquakes = lazy(() => import('./earthquakes/Earthquakes'));
 const ExpensesCar = lazy(() => import('./expenses/Car'));
 const ExpensesTravel = lazy(() => import('./expenses/Travel'));
-const ExpenseTravelGroups = lazy(() => import("./expenses/ExpenseGroups"));
-const FinancesAccountsTransactions = lazy(() => import ("./finances/Accounts/Transactions/Transactions"));
-const FinancesCategorize = lazy(() => import ("./finances/Accounts/Categorize/Categorize"));
-const FinancesCreditDetails = lazy(() => import ("./finances/Credit/Details"));
-const FinancesCalculator = lazy(() => import ("./finances/Credit/Calculator"));
-const FinancesPayments = lazy(() => import ("./finances/Credit/Payments/Payments"));
-const FinancesTimetables = lazy(() => import ("./finances/Credit/Timetables/Timetables"));
-const FXRates = lazy(() => import ("./exchange-rates/ExchangeRates"));
-const InvestmentsBonds = lazy(() => import ("./finances/Investments/Bonds"));
+const ExpenseTravelGroups = lazy(() => import('./expenses/ExpenseGroups'));
+const FinancesAccountsTransactions = lazy(
+  () => import('./finances/Accounts/Transactions/Transactions')
+);
+const FinancesCategorize = lazy(
+  () => import('./finances/Accounts/Categorize/Categorize')
+);
+const FinancesCreditDetails = lazy(() => import('./finances/Credit/Details'));
+const FinancesCalculator = lazy(() => import('./finances/Credit/Calculator'));
+const FinancesPayments = lazy(
+  () => import('./finances/Credit/Payments/Payments')
+);
+const FinancesTimetables = lazy(
+  () => import('./finances/Credit/Timetables/Timetables')
+);
+const FXRates = lazy(() => import('./finances/ExchangeRates'));
+const InvestmentsBonds = lazy(() => import('./finances/Investments/Bonds'));
 const InvestmentsCrypto = lazy(() => import('./finances/Investments/Crypto'));
-const InvestmentsDeposits = lazy(() => import ("./finances/Investments/Deposits"));
-const InvestmentsPension = lazy(() => import ("./finances/Investments/Pension"));
-const InvestmentsStocks = lazy(() => import ("./finances/Investments/Stocks"));
+const InvestmentsDeposits = lazy(
+  () => import('./finances/Investments/Deposits')
+);
+const InvestmentsPension = lazy(() => import('./finances/Investments/Pension'));
+const InvestmentsStocks = lazy(() => import('./finances/Investments/Stocks'));
 const InvestmentsSummary = lazy(() => import('./finances/Investments/Summary'));
 const Logs = lazy(() => import('./apps/Logs'));
 const Meals = lazy(() => import('./meals/Meals'));
 const Messages = lazy(() => import('./messages/Messages'));
-const Profile = lazy(() => import ("./profile/Profile"));
+const Profile = lazy(() => import('./profile/Profile'));
 const Sources = lazy(() => import('./sources/Sources'));
 const Tasks = lazy(() => import('./commands/Tasks'));
 const Todo = lazy(() => import('./apps/TodoList'));
 const Transport = lazy(() => import('./public/Transport'));
-const UserGroups = lazy(() => import("./users/Groups"));
+const UserGroups = lazy(() => import('./users/Groups'));
 const Users = lazy(() => import('./users/Users'));
 const Watchers = lazy(() => import('./commands/Watchers'));
 
@@ -55,73 +65,160 @@ const Error500 = lazy(() => import('./error-pages/Error500'));
 
 const Login = lazy(() => import('./user-pages/Login'));
 const Register1 = lazy(() => import('./user-pages/Register'));
-const TermsAndConditions = lazy(() => import('./user-pages/TermsAndConditions'));
-
+const TermsAndConditions = lazy(
+  () => import('./user-pages/TermsAndConditions')
+);
 
 const AppRoutes = () => {
-  const user = useSelector((state) => state.auth.user)
+  const user = useSelector((state) => state.auth.user);
 
   return (
-    <Suspense fallback={<Spinner/>}>
+    <Suspense fallback={<Spinner />}>
       <Switch>
-        <Route exact path="/terms-and-conditions" component={ TermsAndConditions } />
-        <Route exact path="/expenses/travel/groups" component={ ExpenseTravelGroups } />
-        <Route exact path="/expenses/travel/my" component={ ExpensesTravel } />
-        <Route exact path="/earthquakes" component={ Earthquakes } />
-        <Route exact path="/meals" component={ Meals } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/transport" component={ Transport } />
+        <Route
+          exact
+          path='/terms-and-conditions'
+          component={TermsAndConditions}
+        />
+        <Route
+          exact
+          path='/expenses/travel/groups'
+          component={ExpenseTravelGroups}
+        />
+        <Route exact path='/expenses/travel/my' component={ExpensesTravel} />
+        <Route exact path='/earthquakes' component={Earthquakes} />
+        <Route exact path='/meals' component={Meals} />
+        <Route exact path='/profile' component={Profile} />
+        <Route exact path='/transport' component={Transport} />
 
-        {user?.is_staff && <Route exact path="/" component={Dashboard}/>}
-        {user?.is_staff && <Route exact path="/apps/logs" component={ Logs } />}
-        {user?.is_staff && <Route exact path="/apps/messages" component={ Messages } />}
-        {user?.is_staff && <Route exact path="/apps/todo" component={ Todo } />}
-        {user?.is_staff && <Route exact path="/auth/groups" component={ UserGroups } />}
-        {user?.is_staff && <Route exact path="/auth/users" component={ Users } />}
-        {user?.is_staff && <Route exact path="/bots" component={ Bots } />}
-        {user?.is_staff && <Route exact path="/commands/crons" component={ Crons } />}
-        {user?.is_staff && <Route exact path="/commands/management" component={ Commands } />}
-        {user?.is_staff && <Route exact path="/commands/tasks" component={ Tasks } />}
-        {user?.is_staff && <Route exact path="/commands/watchers" component={ Watchers } />}
-        {user?.is_staff && <Route exact path="/credit/calculator" component={ FinancesCalculator } />}
-        {user?.is_staff && <Route exact path="/credit/details" component={ FinancesCreditDetails } />}
-        {user?.is_staff && <Route exact path="/credit/payments" component={ FinancesPayments } />}
-        {user?.is_staff && <Route exact path="/credit/timetables" component={ FinancesTimetables } />}
-        {user?.is_staff && <Route exact path="/devices" component={ Devices } />}
-        {user?.is_staff && <Route exact path="/expenses/car" component={ ExpensesCar } />}
-        {user?.is_staff && <Route exact path="/finances/accounts/transactions/:id?" component={ FinancesAccountsTransactions } />}
-        {user?.is_staff && <Route exact path="/finances/accounts/categorize" component={ FinancesCategorize } />}
-        {user?.is_staff && <Route exact path="/fx-rates" component={ FXRates } />}
-        {user?.is_staff && <Route exact path="/investments/bonds" component={ InvestmentsBonds } />}
-        {user?.is_staff && <Route exact path="/investments/crypto" component={ InvestmentsCrypto } />}
-        {user?.is_staff && <Route exact path="/investments/deposits" component={ InvestmentsDeposits } />}
-        {user?.is_staff && <Route exact path="/investments/pension" component={ InvestmentsPension } />}
-        {user?.is_staff && <Route exact path="/investments/stocks" component={ InvestmentsStocks } />}
-        {user?.is_staff && <Route exact path="/investments/summary" component={ InvestmentsSummary } />}
-        {user?.is_staff && <Route exact path="/sources" component={ Sources } />}
+        {user?.is_staff && <Route exact path='/' component={Dashboard} />}
+        {user?.is_staff && <Route exact path='/apps/logs' component={Logs} />}
+        {user?.is_staff && (
+          <Route exact path='/apps/messages' component={Messages} />
+        )}
+        {user?.is_staff && <Route exact path='/apps/todo' component={Todo} />}
+        {user?.is_staff && (
+          <Route exact path='/auth/groups' component={UserGroups} />
+        )}
+        {user?.is_staff && <Route exact path='/auth/users' component={Users} />}
+        {user?.is_staff && <Route exact path='/bots' component={Bots} />}
+        {user?.is_staff && (
+          <Route exact path='/commands/crons' component={Crons} />
+        )}
+        {user?.is_staff && (
+          <Route exact path='/commands/management' component={Commands} />
+        )}
+        {user?.is_staff && (
+          <Route exact path='/commands/tasks' component={Tasks} />
+        )}
+        {user?.is_staff && (
+          <Route exact path='/commands/watchers' component={Watchers} />
+        )}
+        {user?.is_staff && (
+          <Route
+            exact
+            path='/credit/calculator'
+            component={FinancesCalculator}
+          />
+        )}
+        {user?.is_staff && (
+          <Route
+            exact
+            path='/credit/details'
+            component={FinancesCreditDetails}
+          />
+        )}
+        {user?.is_staff && (
+          <Route exact path='/credit/payments' component={FinancesPayments} />
+        )}
+        {user?.is_staff && (
+          <Route
+            exact
+            path='/credit/timetables'
+            component={FinancesTimetables}
+          />
+        )}
+        {user?.is_staff && <Route exact path='/devices' component={Devices} />}
+        {user?.is_staff && (
+          <Route exact path='/expenses/car' component={ExpensesCar} />
+        )}
+        {user?.is_staff && (
+          <Route
+            exact
+            path='/finances/accounts/transactions/:id?'
+            component={FinancesAccountsTransactions}
+          />
+        )}
+        {user?.is_staff && (
+          <Route
+            exact
+            path='/finances/accounts/categorize'
+            component={FinancesCategorize}
+          />
+        )}
+        {user?.is_staff && <Route exact path='/fx-rates' component={FXRates} />}
+        {user?.is_staff && (
+          <Route exact path='/investments/bonds' component={InvestmentsBonds} />
+        )}
+        {user?.is_staff && (
+          <Route
+            exact
+            path='/investments/crypto'
+            component={InvestmentsCrypto}
+          />
+        )}
+        {user?.is_staff && (
+          <Route
+            exact
+            path='/investments/deposits'
+            component={InvestmentsDeposits}
+          />
+        )}
+        {user?.is_staff && (
+          <Route
+            exact
+            path='/investments/pension'
+            component={InvestmentsPension}
+          />
+        )}
+        {user?.is_staff && (
+          <Route
+            exact
+            path='/investments/stocks'
+            component={InvestmentsStocks}
+          />
+        )}
+        {user?.is_staff && (
+          <Route
+            exact
+            path='/investments/summary'
+            component={InvestmentsSummary}
+          />
+        )}
+        {user?.is_staff && <Route exact path='/sources' component={Sources} />}
 
-        <Route path="/basic-ui/buttons" component={ Buttons } />
-        <Route path="/basic-ui/dropdowns" component={ Dropdowns } />
-        <Route path="/basic-ui/typography" component={ Typography } />
+        <Route path='/basic-ui/buttons' component={Buttons} />
+        <Route path='/basic-ui/dropdowns' component={Dropdowns} />
+        <Route path='/basic-ui/typography' component={Typography} />
 
-        <Route path="/form-Elements/basic-elements" component={ BasicElements } />
+        <Route path='/form-Elements/basic-elements' component={BasicElements} />
 
-        <Route path="/tables/basic-table" component={ BasicTable } />
+        <Route path='/tables/basic-table' component={BasicTable} />
 
-        <Route path="/icons/mdi" component={ Mdi } />
+        <Route path='/icons/mdi' component={Mdi} />
 
-        <Route path="/charts/chart-js" component={ ChartJs } />
+        <Route path='/charts/chart-js' component={ChartJs} />
 
-        <Route path="/login" component={ Login } />
-        <Route path="/register" component={ Register1 } />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register1} />
 
-        <Route path="/error-pages/error-404" component={ Error404 } />
-        <Route path="/error-pages/error-500" component={ Error500 } />
+        <Route path='/error-pages/error-404' component={Error404} />
+        <Route path='/error-pages/error-500' component={Error500} />
 
-        <Redirect to="/error-pages/error-404" />
+        <Redirect to='/error-pages/error-404' />
       </Switch>
     </Suspense>
   );
-}
+};
 
 export default AppRoutes;
--- frontend/src/app/shared/BottomPagination.js
+++ frontend/src/app/shared/BottomPagination.js
@@ -1,103 +1,100 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Pagination } from "react-bootstrap";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Pagination } from 'react-bootstrap';
 
-const BottomPagination = props => {
+const BottomPagination = (props) => {
   const dispatch = useDispatch();
-  const kwargs = props.items.kwargs || {}
+  const kwargs = props.items.kwargs || {};
   const currentPage = !props.items.previous
     ? 1
-    : (parseInt(new URL(props.items.previous).searchParams.get("page")) || 1) + 1
-  const pageSize = props.items.page_size
-  const lastPage = Math.ceil(props.items.count / pageSize)
-  const token = useSelector((state) => state.auth.token)
+    : (parseInt(new URL(props.items.previous).searchParams.get('page')) || 1) +
+      1;
+  const pageSize = props.items.page_size;
+  const lastPage = Math.ceil(props.items.count / pageSize);
+  const token = useSelector((state) => state.auth.token);
   useEffect(() => {
-    if (props.newApi)
-      dispatch(props.fetchMethod(kwargs))
-    else
-      dispatch(props.fetchMethod(token, kwargs));
+    if (props.newApi) dispatch(props.fetchMethod(kwargs));
+    else dispatch(props.fetchMethod(token, kwargs));
   }, [kwargs]);
-  return props.items.count > pageSize
-    ? <Pagination>
+  return props.items.count > pageSize ? (
+    <Pagination>
       <Pagination.First
         disabled={!props.items.previous}
-        onClick={() => dispatch(props.setKwargs({page: 1}))}
+        onClick={() => dispatch(props.setKwargs({ page: 1 }))}
       />
       <Pagination.Prev
         disabled={!props.items.previous}
-        onClick={() => dispatch(props.setKwargs({page: currentPage -1}))}
+        onClick={() => dispatch(props.setKwargs({ page: currentPage - 1 }))}
       />
-      {
-        currentPage > 1 && <Pagination.Item
+      {currentPage > 1 && (
+        <Pagination.Item
           disabled={currentPage < 2}
-          onClick={() => dispatch(props.setKwargs({page: 1}))}
+          onClick={() => dispatch(props.setKwargs({ page: 1 }))}
         >
           1
         </Pagination.Item>
-      }
-      {
-        currentPage > 2 && <Pagination.Item
+      )}
+      {currentPage > 2 && (
+        <Pagination.Item
           disabled={currentPage < 3}
-          onClick={() => dispatch(props.setKwargs({page: 2}))}
-          linkClassName="bg-transparent"
+          onClick={() => dispatch(props.setKwargs({ page: 2 }))}
+          linkClassName='bg-transparent'
         >
           2
         </Pagination.Item>
-      }
-      {
-        currentPage - 8 > 0 && <Pagination.Ellipsis />
-      }
-      {
-        Array.from(new Array(5), (x, i) => i + currentPage - 6).filter(i => i + 1 > 2).map(i =>
+      )}
+      {currentPage - 8 > 0 && <Pagination.Ellipsis />}
+      {Array.from(new Array(5), (x, i) => i + currentPage - 6)
+        .filter((i) => i + 1 > 2)
+        .map((i) => (
           <Pagination.Item
             active={i + 1 === currentPage}
             key={i}
-            onClick={() => dispatch(props.setKwargs({page: i+1}))}
+            onClick={() => dispatch(props.setKwargs({ page: i + 1 }))}
           >
-            {i+1}
-          </Pagination.Item>)
-      }
+            {i + 1}
+          </Pagination.Item>
+        ))}
 
       <Pagination.Item active>{currentPage}</Pagination.Item>
 
-      {
-        Array.from(new Array(5), (x, i) => i + currentPage).filter(i => i < lastPage).map(i =>
+      {Array.from(new Array(5), (x, i) => i + currentPage)
+        .filter((i) => i < lastPage)
+        .map((i) => (
           <Pagination.Item
             active={i + 1 === currentPage}
             key={i}
-            onClick={() => dispatch(props.setKwargs({page: i+1}))}
+            onClick={() => dispatch(props.setKwargs({ page: i + 1 }))}
           >
-            {i+1}
-          </Pagination.Item>)
-      }
-      {
-        currentPage + 7 < lastPage && <Pagination.Ellipsis />
-      }
-      {
-        lastPage - 6 > currentPage && <Pagination.Item
+            {i + 1}
+          </Pagination.Item>
+        ))}
+      {currentPage + 7 < lastPage && <Pagination.Ellipsis />}
+      {lastPage - 6 > currentPage && (
+        <Pagination.Item
           disabled={currentPage > lastPage - 1}
-          onClick={() => dispatch(props.setKwargs({page: lastPage - 1}))}
+          onClick={() => dispatch(props.setKwargs({ page: lastPage - 1 }))}
         >
           {lastPage - 1}
         </Pagination.Item>
-      }
-      {
-        lastPage - 5 > currentPage && <Pagination.Item
+      )}
+      {lastPage - 5 > currentPage && (
+        <Pagination.Item
           active={lastPage - 1 === currentPage}
-          onClick={() => dispatch(props.setKwargs({page: lastPage}))}
+          onClick={() => dispatch(props.setKwargs({ page: lastPage }))}
         >
           {lastPage}
         </Pagination.Item>
-      }
+      )}
       <Pagination.Next
         disabled={!props.items.next}
-        onClick={() => dispatch(props.setKwargs({page: currentPage + 1}))}
+        onClick={() => dispatch(props.setKwargs({ page: currentPage + 1 }))}
       />
       <Pagination.Last
         disabled={!props.items.next}
-        onClick={() => dispatch(props.setKwargs({page: lastPage}))}
+        onClick={() => dispatch(props.setKwargs({ page: lastPage }))}
       />
     </Pagination>
-    : null
-}
-export default BottomPagination
+  ) : null;
+};
+export default BottomPagination;
