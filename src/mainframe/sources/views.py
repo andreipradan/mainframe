5db269cd4619eaa1114d5fb3212a107c7dd60900
Andrei <work@pradan.dev>
2026-06-27 15:09:47 +0300

    be favorite bands

--- src/mainframe/sources/views.py
+++ src/mainframe/sources/views.py
@@ -10,6 +10,12 @@ class SourcesViewSet(viewsets.ModelViewSet):
     serializer_class = SourceSerializer
     permission_classes = (IsAuthenticated,)
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.query_params.get("type"):
+            return queryset.filter(type=self.request.query_params.get("type"))
+        return queryset
+
     def list(self, request, *args, **kwargs):
         response = super().list(request, *args, **kwargs)
         response.data["types"] = Source.TYPES
